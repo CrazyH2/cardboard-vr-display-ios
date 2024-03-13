f35c2aeba6d7a38d7af117126168efacb046b0b5
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-13 13:44:25 +0000

    add more headsets (daydream)

--- src/device-info.js
+++ src/device-info.js
@@ -67,6 +67,30 @@ var Viewers = {
     inverseCoefficients: [-0.33836704, -0.18162185, 0.862655, -1.2462051,
       1.0560602, -0.58208317, 0.21609078, -0.05444823, 0.009177956,
       -9.904169E-4, 6.183535E-5, -1.6981803E-6]
+  }),
+  DaydreamViewV1: new CardboardViewer({
+    id: 'DaydreamViewV1',
+    label: 'Daydream I/O 2016',
+    fov: 60,
+    interLensDistance: 0.064,
+    baselineLensDistance: 0.035,
+    screenLensDistance: 0.039,
+    distortionCoefficients: [0.34, 0.55],
+    inverseCoefficients: [-0.33836704, -0.18162185, 0.862655, -1.2462051,
+      1.0560602, -0.58208317, 0.21609078, -0.05444823, 0.009177956,
+      -9.904169E-4, 6.183535E-5, -1.6981803E-6]
+  }),
+  DaydreamViewV2: new CardboardViewer({
+    id: 'DaydreamViewV2',
+    label: 'Daydream I/O 2017',
+    fov: 60,
+    interLensDistance: 0.064,
+    baselineLensDistance: 0.035,
+    screenLensDistance: 0.039,
+    distortionCoefficients: [0.34, 0.55],
+    inverseCoefficients: [-0.33836704, -0.18162185, 0.862655, -1.2462051,
+      1.0560602, -0.58208317, 0.21609078, -0.05444823, 0.009177956,
+      -9.904169E-4, 6.183535E-5, -1.6981803E-6]
   })
 };
 
