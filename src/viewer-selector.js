d60bc384b8aafe52702c370a03d3793010e76a0b
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-14 07:57:11 +0000

    udpate selector ui

--- src/viewer-selector.js
+++ src/viewer-selector.js
@@ -133,10 +133,10 @@ ViewerSelector.prototype.createDialog_ = function(options) {
   s.width = width + 'px';
   s.padding = '24px';
   s.overflow = 'hidden';
-  s.background = '#161618';
+  s.background = '#212124';
   s.borderRadius = '5px';
   s.fontFamily = "'Roboto', sans-serif";
-  s.boxShadow = '0px 5px 20px #666';
+  s.boxShadow = '0px 5px 20px #212124';
 
   dialog.appendChild(this.createH1_('Select your viewer'));
   for (var id in options) {
@@ -171,7 +171,7 @@ ViewerSelector.prototype.createChoice_ = function(id, name) {
   */
   var div = document.createElement('div');
   div.style.marginTop = '8px';
-  div.style.color = 'white';
+  div.style.color = '#818181';
 
   var input = document.createElement('input');
   input.style.fontSize = '30px';
