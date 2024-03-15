52f03d4033883c06f511010c21e9eac1f404c833
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-15 08:51:16 +0000

    s

--- dist/cardboard-vr-display.js
+++ dist/cardboard-vr-display.js
@@ -2533,7 +2533,7 @@ function RotateInstructions() {
   s.bottom = '22px';
   s.borderRadius = '10px';
   s.marginLeft = '12px';
-  s.width = 'calc(100% - calc(12 * 2))';
+  s.width = 'calc(100% - 12)';
   s.height = '48px';
   s.padding = '14px 24px';
   s.boxSizing = 'border-box';
